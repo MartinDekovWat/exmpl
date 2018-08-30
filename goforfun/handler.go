1784ffd4dc020d39fb4d5e64a3598c56d136907f
MartinDekovWat <42210992+MartinDekovWat@users.noreply.github.com>
2018-08-30 06:41:02 -0700

    Update handler.go

--- goforfun/handler.go
+++ goforfun/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("%s yo we cool alejuah?", string(req))
+	return fmt.Sprintf("%s swag is the way to gow?", string(req))
 }
